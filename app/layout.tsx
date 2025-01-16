670eeb9e861b3e46b919a36e5a1256c8a5af7de5
keal khondekar <kewalkhondekar25@gmail.com>
2025-01-16 18:49:06 +0530

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html suppressHydrationWarning={true} lang="en">
       <body
+        suppressHydrationWarning={true}
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
